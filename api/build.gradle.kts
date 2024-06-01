04afba32615a9af53f3d6b949cb5abf7dcd3e52d
Alexandre Aragão <alexr.aragao@gmail.com>
2024-06-01 16:13:10 -0300

    feat(api): add spring-boot-starter-security dependency

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -26,6 +26,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -40,6 +41,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
