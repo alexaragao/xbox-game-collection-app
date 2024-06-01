ace9569a26dc8adf989802d0474e40ad2560ee68
Alexandre Aragão <alexr.aragao@gmail.com>
2024-06-01 15:50:15 -0300

    init(app): first commit

---
+++ app/settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "X360-Collection"
+include(":composeApp")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
