faec7bfeffb7a3d15ffd497535c541dce3180cca
Alexandre Aragão <alexr.aragao@gmail.com>
2024-06-01 16:15:37 -0300

    feat(api): add auth0:java-jwt dependency

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -31,12 +31,15 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
 
     compileOnly("org.projectlombok:lombok")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
     annotationProcessor("org.projectlombok:lombok")
 
