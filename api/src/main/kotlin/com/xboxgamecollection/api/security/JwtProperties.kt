54180bc0e15eca961c379e9008ffb9a31819e808
Alexandre Aragão <alexr.aragao@gmail.com>
2024-06-04 12:19:55 -0300

    feat(api): create jwt properties security file

---
+++ api/src/main/kotlin/com/xboxgamecollection/api/security/JwtProperties.kt
@@ -0,0 +1,10 @@
+package com.xboxgamecollection.api.security
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("jwt")
+data class JwtProperties(
+    val key: String,
+    val accessTokenExpiration: Long,
+    val refreshTokenExpiration: Long,
+)
