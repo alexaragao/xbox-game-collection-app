05a416a6f6e4539371ce884cb77029cb3585890f
Alexandre Aragão <alexr.aragao@gmail.com>
2024-06-13 16:49:47 -0300

    feat(app): create get game by id use case

--- app/composeApp/src/commonMain/kotlin/com/xboxgamecollection/app/features/game/di/GameModule.kt
+++ app/composeApp/src/commonMain/kotlin/com/xboxgamecollection/app/features/game/di/GameModule.kt
@@ -4,6 +4,7 @@ import com.xboxgamecollection.app.features.game.data.repository.GameRepository
 import com.xboxgamecollection.app.features.game.data.service.GameService
 import com.xboxgamecollection.app.features.game.domain.repository.IGameRepository
 import com.xboxgamecollection.app.features.game.domain.service.IGameService
+import com.xboxgamecollection.app.features.game.domain.usecase.GetGameByIdUseCase
 import com.xboxgamecollection.app.network.services.apiClient
 import org.koin.dsl.module
 
@@ -11,4 +12,5 @@ val gameModule = module {
     single { apiClient }
     single<IGameService> { GameService(get()) }
     single<IGameRepository> { GameRepository(get()) }
+    single { GetGameByIdUseCase(get()) }
 }
