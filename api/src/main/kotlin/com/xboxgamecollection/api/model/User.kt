5ebf6275f582762643a4f0489260a344bf63feba
Alexandre Aragão <alexr.aragao@gmail.com>
2024-06-13 16:27:18 -0300

    feat(api): update User id to UUID

--- api/src/main/kotlin/com/xboxgamecollection/api/model/User.kt
+++ api/src/main/kotlin/com/xboxgamecollection/api/model/User.kt
@@ -1,14 +1,21 @@
 package com.xboxgamecollection.api.model
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.annotations.ColumnDefault
+import org.hibernate.annotations.UuidGenerator
 import java.time.LocalDateTime
-import jakarta.persistence.*
+import java.util.UUID
 
 @Entity
 @Table(name = "users")
 data class User(
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    val id: Long = 0,
+    @UuidGenerator
+    @ColumnDefault("gen_random_uuid()")
+    val id: UUID = UUID.randomUUID(),
 
     @Column(name = "nickname", nullable = false)
     val nickname: String,
@@ -17,8 +24,13 @@ data class User(
     val password: String,
 
     @Column(name = "created_at", nullable = false)
+    @ColumnDefault("now()")
     val createdAt: LocalDateTime = LocalDateTime.now(),
 
     @Column(name = "updated_at", nullable = false)
+    @ColumnDefault("now()")
     val updatedAt: LocalDateTime = LocalDateTime.now(),
+
+//    @OneToMany(mappedBy = "users", cascade = [CascadeType.ALL], orphanRemoval = true)
+//    val games: List<UserGameCollection> = mutableListOf()
 )
