aac4c23e7243664046b5ad79e5f4ffa8f427a45d
Alexandre Aragão <alexr.aragao@gmail.com>
2024-06-13 16:49:04 -0300

    feat(app): create get game module

---
+++ app/composeApp/src/commonMain/kotlin/com/xboxgamecollection/app/features/game/di/GameModule.kt
@@ -0,0 +1,14 @@
+package com.xboxgamecollection.app.features.game.di
+
+import com.xboxgamecollection.app.features.game.data.repository.GameRepository
+import com.xboxgamecollection.app.features.game.data.service.GameService
+import com.xboxgamecollection.app.features.game.domain.repository.IGameRepository
+import com.xboxgamecollection.app.features.game.domain.service.IGameService
+import com.xboxgamecollection.app.network.services.apiClient
+import org.koin.dsl.module
+
+val gameModule = module {
+    single { apiClient }
+    single<IGameService> { GameService(get()) }
+    single<IGameRepository> { GameRepository(get()) }
+}
