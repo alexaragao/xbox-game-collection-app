6d3d790e6355f41a2c8f68bb42635d03a5b87e2f
Vinicius Anchieta <vinicius.anchieta@outlook.com>
2024-06-14 16:03:34 -0300

    chore(api): create method search in repository

--- api/src/main/kotlin/com/xboxgamecollection/api/repository/GameRepository.kt
+++ api/src/main/kotlin/com/xboxgamecollection/api/repository/GameRepository.kt
@@ -2,7 +2,12 @@ package com.xboxgamecollection.api.repository
 
 import com.xboxgamecollection.api.model.Game
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface GameRepository : JpaRepository<Game, Long>
+interface GameRepository : JpaRepository<Game, Long> {
+    @Query("SELECT g FROM Game g WHERE g.title LIKE %:search% AND g.genre = :genre")
+    fun search(@Param("search") search: String, @Param("genre") genre: String): List<Game>
+}
--- api/src/main/kotlin/com/xboxgamecollection/api/service/GameService.kt
+++ api/src/main/kotlin/com/xboxgamecollection/api/service/GameService.kt
@@ -2,20 +2,16 @@ package com.xboxgamecollection.api.service
 
 import com.xboxgamecollection.api.model.Game
 import com.xboxgamecollection.api.repository.GameRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Service
 
 @Service
 class GameService(
     private val gameRepository: GameRepository
 ) {
-
-    fun findAll(
-        title: String?,
-        genre: String?
-    ): List<Game> = gameRepository.findAll().filter { game ->
-        (title == null || game.title.contains(title, ignoreCase = true)) &&
-        (genre == null || game.genre.contains(genre, ignoreCase = true))
-    }
+    fun search(search: String?, genre: String?): List<Game> = gameRepository.search(search ?: "", genre ?: "")
+    
+    fun findAll(): List<Game> = gameRepository.findAll()
 
     fun findById(id: Long): Game? = gameRepository.findById(id).orElse(null)
 
