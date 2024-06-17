a315530ad1b28c31c1aa47c1739e75fe7a89ca1c
Alexandre Aragão <alexr.aragao@gmail.com>
2024-06-17 14:01:49 -0300

    feat(app): rename game details screen title

--- app/composeApp/src/commonMain/kotlin/com/xboxgamecollection/app/navigation/AppScreen.kt
+++ app/composeApp/src/commonMain/kotlin/com/xboxgamecollection/app/navigation/AppScreen.kt
@@ -3,7 +3,7 @@ package com.xboxgamecollection.app.navigation
 enum class AppScreen(val title: String) {
     Home(title = "app_home"),
     GameList(title = "app_game_list"),
-    GameDetail(title = "app_game_detail"),
+    GameDetails(title = "app_game_details/{gameId}"),
     Profile(title = "app_profile"),
     SignIn(title = "app_sign_in"),
     Register(title = "app_register"),
