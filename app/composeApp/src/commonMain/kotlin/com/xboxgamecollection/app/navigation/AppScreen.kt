c2477f1e7565c323da9812d79b0b0d29d7106aa0
Alexandre Aragão <alexr.aragao@gmail.com>
2024-06-17 11:25:18 -0300

    feat(app): add auth routes to app screen

--- app/composeApp/src/commonMain/kotlin/com/xboxgamecollection/app/navigation/AppScreen.kt
+++ app/composeApp/src/commonMain/kotlin/com/xboxgamecollection/app/navigation/AppScreen.kt
@@ -4,4 +4,6 @@ enum class AppScreen(val title: String) {
     Home(title = "app_home"),
     GameList(title = "app_game_list"),
     GameDetail(title = "app_game_detail")
+    SignIn(title = "app_sign_in"),
+    Register(title = "app_register"),
 }
