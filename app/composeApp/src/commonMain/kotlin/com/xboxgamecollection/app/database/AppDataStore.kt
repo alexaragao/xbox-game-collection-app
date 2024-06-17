583ea84a01f7910646589be46c3bc89deca8d151
Alexandre Aragão <alexr.aragao@gmail.com>
2024-06-17 11:23:04 -0300

    feat(app): create base app datastore

---
+++ app/composeApp/src/commonMain/kotlin/com/xboxgamecollection/app/database/AppDataStore.kt
@@ -0,0 +1,15 @@
+package com.xboxgamecollection.app.database
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import okio.Path.Companion.toPath
+
+internal const val dataStoreFileName = "app.preferences_pb"
+
+internal expect fun createDataStore(): DataStore<Preferences>
+
+fun createDataStore(producePath: () -> String): DataStore<Preferences> =
+    PreferenceDataStoreFactory.createWithPath(
+        produceFile = { producePath().toPath() }
+    )
