a9d8ae62d9cd2301864a121b8c5938513219cf0b
Alexandre Aragão <alexr.aragao@gmail.com>
2024-06-17 11:51:57 -0300

    feat(api): create fet game by id endpoint

--- api/src/main/kotlin/com/xboxgamecollection/api/controller/GameController.kt
+++ api/src/main/kotlin/com/xboxgamecollection/api/controller/GameController.kt
@@ -2,8 +2,14 @@ package com.xboxgamecollection.api.controller
 
 import com.xboxgamecollection.api.model.Game
 import com.xboxgamecollection.api.service.GameService
-import org.springframework.data.jpa.repository.Query
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/games")
@@ -14,6 +20,18 @@ class GameController(val gameService: GameService) {
         @RequestParam genre: String?
     ): List<Game> {
         return gameService.search(search, genre)
+    }
+
+    @GetMapping("/{gameId}")
+    fun getById(@PathVariable gameId: String): ResponseEntity<Game> {
+        val gameUUID = UUID.fromString(gameId)
+        val game = gameService.findById(gameUUID)
+
+        return if (game != null) {
+            ResponseEntity.ok(game)
+        } else {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
+        }
     }
 
 }
