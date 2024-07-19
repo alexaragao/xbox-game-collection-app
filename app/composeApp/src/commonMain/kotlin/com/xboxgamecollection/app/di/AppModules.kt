d46394d089c77026fcc1e53dad6a81bd30b18da2
Alexandre Aragão <alexr.aragao@gmail.com>
2024-07-19 13:50:56 -0300

    feat(app): add user game features feature to koin modules

--- app/composeApp/src/commonMain/kotlin/com/xboxgamecollection/app/di/AppModules.kt
+++ app/composeApp/src/commonMain/kotlin/com/xboxgamecollection/app/di/AppModules.kt
@@ -3,9 +3,11 @@ package com.xboxgamecollection.app.di
 import com.xboxgamecollection.app.features.auth.di.authenticationModule
 import com.xboxgamecollection.app.features.game.di.gameModule
 import com.xboxgamecollection.app.features.gameBarcode.di.gameBarcodeModule
+import com.xboxgamecollection.app.features.userCollection.di.userCollectionModule
 
 val appModules = listOf(
     authenticationModule,
     gameModule,
-    gameBarcodeModule
+    gameBarcodeModule,
+    userCollectionModule,
 )
